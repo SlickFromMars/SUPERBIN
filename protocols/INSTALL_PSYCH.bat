0fe1b2fe9c45f577cee7bfa86ae61e60bda6156a
SlickFromMars <82390475+SlickFromMars@users.noreply.github.com>
2022-08-26 08:04:59 -0400

    Update INSTALL_PSYCH.bat

--- protocols/INSTALL_PSYCH.bat
+++ protocols/INSTALL_PSYCH.bat
@@ -5,6 +5,5 @@ echo.
 start chrome https://github.com/ShadowMario/FNF-PsychEngine/releases/download/0.6.2/PsychEngine-Windows64.zip
 echo ZIP has been downloaded.
 
-echo Installation complete. Have fun!
 pause
 explorer.exe %USERPROFILE%\Downloads
