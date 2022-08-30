3e87e0798322933cc814ce1aa1183aab29806259
SlickFromMars <82390475+SlickFromMars@users.noreply.github.com>
2022-08-30 13:18:52 -0400

    optional FNF libraries

--- haxe/HAXE_SETUP.bat
+++ haxe/HAXE_SETUP.bat
@@ -1,5 +1,6 @@
 @echo off
-echo Installing Haxe Libraries...
+title Haxeflixel Setup (Basic Libraries)
+echo Installing Basic Haxe Libraries...
 echo.
 
 haxelib install lime
@@ -13,6 +14,19 @@ haxelib install actuate
 haxelib run lime setup
 haxelib run lime setup flixel
 haxelib run flixel-tools setup
+
+title Haxeflixel Setup
+echo.
+:choice
+set /P c=Basic install complete. Proceed to FNF libraries?[Y/N]?
+if /I "%c%" EQU "Y" goto :fnf
+if /I "%c%" EQU "N" goto :verify
+
+goto :choice
+
+title Haxeflixel Setup (FNF Libraries)
+
+:fnf
 haxelib git linc_luajit https://github.com/nebulazorua/linc_luajit.git
 haxelib git hxvm-luajit https://github.com/nebulazorua/hxvm-luajit
 haxelib git faxe https://github.com/uhrobots/faxe
@@ -25,12 +39,9 @@ haxelib install hxjsonast
 haxelib install hxCodec
 haxelib git hscript-ex https://github.com/ianharrigan/hscript-ex
 haxelib install hxcpp-debug-server
-haxelib install slick_addons
 
-echo.
-echo Updating libaries...
-haxelib update
-
+:verify
+title Haxeflixel Setup
 echo.
 haxelib list
 
---
+++ haxe/HAXE_UPDATE.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Updating Haxe Libraries...
+echo.
+
+haxelib update
+
+echo.
+haxelib list
+
+echo.
+echo Update complete.
+pause
